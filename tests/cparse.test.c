03117ddc6adac8d174345a15a0c5edc838d6067c
Ryan Jennings <c0der78@gmail.com>
2015-07-25 01:01:43 -0700

    remove c++ comments

--- tests/cparse.test.c
+++ tests/cparse.test.c
@@ -51,11 +51,11 @@ int main(void)
 
     sr = srunner_create(cparse_parse_suite());
     srunner_add_suite(sr, cparse_json_suite());
-    //srunner_add_suite(sr, cparse_object_suite());
-    //srunner_add_suite(sr, cparse_query_suite());
+    srunner_add_suite(sr, cparse_object_suite());
+    srunner_add_suite(sr, cparse_query_suite());
     srunner_add_suite(sr, cparse_util_suite());
     srunner_add_suite(sr, cparse_user_suite());
-    //srunner_add_suite(sr, cparse_client_suite());
+    srunner_add_suite(sr, cparse_client_suite());
     srunner_add_suite(sr, cparse_acl_suite());
     srunner_add_suite(sr, cparse_role_suite());
     srunner_run_all (sr, CK_NORMAL);
