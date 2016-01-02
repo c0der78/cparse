912eb79051f0b958cfe50357c2dd365ec946e233
Ryan Jennings <ryan@arg3.com>
2016-01-01 19:57:36 -0800

    fix typo

--- cmake/MemCheck.cmake
+++ cmake/MemCheck.cmake
@@ -1,11 +1,11 @@
 
 find_program(MEMCHECK_COMMAND valgrind)
 
-function(add_memcheck_test _targetname _testrunner)
-  if (NOT MEMCHECK_COMMAND)
-    add_test(${_targetname} ${_testrunner} ${ARGV2})
+function(add_memcheck_test _cond _targetname _testrunner)
+  if (NOT ${_cond} OR NOT MEMCHECK_COMMAND)
+    add_test(${_targetname} ${_testrunner} ${ARGV3})
   else ()
-    add_test(${_targetname} ${MEMCHECK_COMMAND} --leak-check=full --error-exitcode=5 ${ARGV2} --quiet ${_testrunner})
+    add_test(${_targetname} ${MEMCHECK_COMMAND} --leak-check=full --error-exitcode=5 ${ARGV3} --quiet ${_testrunner})
   endif()
 
 endfunction()
